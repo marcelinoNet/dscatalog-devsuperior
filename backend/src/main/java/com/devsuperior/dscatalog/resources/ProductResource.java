744f854ddfb0bad724b09d558566c5f5e9be07c0
marcelinoNet <marcelinoneto34@gmail.com>
2021-12-13 13:26:13 -0300

    FIX: Postgres compatibility

--- backend/src/main/java/com/devsuperior/dscatalog/resources/ProductResource.java
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/ProductResource.java
@@ -32,11 +32,12 @@ public class ProductResource {
 	@GetMapping
 	public ResponseEntity<Page<ProductDTO>> findAll(
 			@RequestParam(value = "categoryId", defaultValue = "0") Long categoryId,
+			@RequestParam(value = "name", defaultValue = "") String name,
 			
 			Pageable pageable){
 		 
 		
-		Page<ProductDTO> pages = service.findAllPaged(categoryId, pageable);
+		Page<ProductDTO> pages = service.findAllPaged(categoryId,name.trim() ,pageable);
 		
 		return ResponseEntity.ok().body(pages);	
 	}
